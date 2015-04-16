eba5a682d554956ae7b3067f947114393c2814db
Laurens Van Houtven <_@lvh.io>
2015-04-16 11:50:29 -0400

    Make the tests pass

--- falcon/cors.py
+++ falcon/cors.py
@@ -1,2 +1,25 @@
+from collections import namedtuple
+
+Parameters = namedtuple('Parameters', ('origin', 'method', 'headers'))
+
 def allow_all_origins(parameters):
     return True
+
+def allow_origins(origins):
+    allowed_origins = frozenset(origins)
+
+    def pred(parameters):
+        return parameters.origin in allowed_origins
+
+    return pred
+
+def allow_all_methods(parameters):
+    return True
+
+def allow_methods(methods):
+    allowed_methods = frozenset(methods)
+
+    def pred(parameters):
+        return parameters.method in allowed_methods
+
+    return pred
