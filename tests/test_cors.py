bb06afc2a0441f8817bbc4f543a41000e08e7a32
Laurens Van Houtven <_@lvh.io>
2015-04-16 11:40:47 -0400

    Tests

---
+++ tests/test_cors.py
@@ -0,0 +1,10 @@
+import testtools
+
+from falcon import cors
+
+class CORSTests(testtools.TestCase):
+    def test_allow_all_origins(self):
+        for p in [cors.Parameters("YOLO", "YOLO", "YOLO")]:
+            self.assertTrue(cors.allow_all_origins(p))
+
+    def test_allow
